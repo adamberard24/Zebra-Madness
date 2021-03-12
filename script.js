a5319c7cec4e97213fcffba9c35443bda4794755
Adam Schwartz <adamberard24@gmail.com>
2021-03-12 00:20:40 -0500

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,4 @@
+const h2 = document.createElement('h2');
+h2.textContent = "You can click on the link below and it will take you to wikipedia! Neat!";
+
+document.querySelector('body').appendChild(h2);
